d1c5f58ec167b7e404aaa15811834d4deeb964c6
Cameron Wong <cwong@g.harvard.edu>
2023-06-22 17:58:10 -0400

    basic framework
    Signed-off-by: Cameron Wong <cwong@g.harvard.edu>

--- src/test/scala/lms/koika/statestruct.scala
+++ src/test/scala/lms/koika/statestruct.scala
@@ -11,26 +11,38 @@ import lms.collection.mutable._
 class StateStructTest extends TutorialFunSuite {
   val under = "structdemo/"
 
+  val MAX_STEPS = 1000
+  val NUM_REGS = 3
+
   override def exec(label: String, code: String, suffix: String = "c") =
     super.exec(label, code, suffix)
 
   override def check(label: String, code: String, suffix: String = "c") =
     super.check(label, code, suffix)
 
-  case class Reg(id: Int)
+  trait Driver extends Dsl with StructOps {
+    case class Reg(id: Int)
 
-  // TODO cwong: The arrays should have fixed size
-  @CStruct case class State(
-    // XXX cwong: Maybe this should be a dedicated [Registers] struct?
-    registers: Array[Int],
-    cache_val: Int,
-    time: Int,
-    mem: Array[Int],
-  )
+    // TODO cwong: The arrays should have fixed size
+    @CStruct case class State(
+      // XXX cwong: Maybe this should be a dedicated [Registers] struct?
+      registers: Array[Int],
+      cache_val: Int,
+      time: Int,
+      mem: Array[Int],
+    )
 
-  abstract sealed class Instruction
-  case class Add(dst: Reg, lsrc: Reg, rsrc: Reg) extends Instruction
-  case class Branch(tst: Reg, target: Reg) extends Instruction
-  case class Load(dst: Reg, offs: Int, src: Reg) extends Instruction
-  case class Store(dst: Int, offs: Int, src: Int) extends Instruction
+    def r(id: Int): Reg = Reg(id)
+
+    abstract sealed class Instruction
+    case class Add(dst: Reg, lsrc: Reg, rsrc: Reg) extends Instruction
+    case class Branch(tst: Reg, target: Reg) extends Instruction
+    case class Load(dst: Reg, offs: Int, src: Reg) extends Instruction
+    case class Store(dst: Reg, offs: Int, src: Reg) extends Instruction
+
+    type Prog = List[Instruction]
+
+    def init(_unit: Rep[Unit]): Rep[State] = {
+    }
+  }
 }
